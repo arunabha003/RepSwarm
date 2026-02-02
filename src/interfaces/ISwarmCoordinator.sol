0a517664ce547ac704b42469060357d25ddbd531
Arunabha <arunabhadhal04@gmail.com>
2026-02-03 02:28:04 +0530

    feat: add AgentInfo struct and getAgentInfo function

--- src/interfaces/ISwarmCoordinator.sol
+++ src/interfaces/ISwarmCoordinator.sol
@@ -13,11 +13,27 @@ interface ISwarmCoordinator {
         uint64 deadline;
         uint16 mevFeeBps;
         uint16 treasuryBps;
+        uint16 lpShareBps;
         bool executed;
     }
+
+    struct AgentInfo {
+        bool approved;
+        uint256 identityId;
+    }
+
+    // Errors
+    error IntentExpired();
+    error IntentAlreadyExecuted(uint256 intentId);
+    error NoCandidates();
+    error NoProposals(uint256 intentId);
+    error AgentNotApproved();
+    error AlreadyProposed();
 
     function getIntent(uint256 intentId) external view returns (IntentView memory);
     function getCandidateCount(uint256 intentId) external view returns (uint256);
     function getCandidatePath(uint256 intentId, uint256 candidateId) external view returns (bytes memory);
+    function getAgentInfo(address agent) external view returns (AgentInfo memory);
+    function getProposalAgents(uint256 intentId) external view returns (address[] memory);
     function submitProposal(uint256 intentId, uint256 candidateId, int256 score, bytes calldata data) external;
 }
