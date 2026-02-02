d13aeeb46d9121f034341a116c78e3e3a654ec52
Arunabha <arunabhadhal04@gmail.com>
2026-02-03 02:26:20 +0530

    feat : add deploy script

--- script/DeploySwarm.s.sol
+++ script/DeploySwarm.s.sol
@@ -8,6 +8,8 @@ import {HookMiner} from "v4-periphery/src/utils/HookMiner.sol";
 
 import {SwarmCoordinator} from "../src/SwarmCoordinator.sol";
 import {MevRouterHook} from "../src/hooks/MevRouterHook.sol";
+import {OracleRegistry} from "../src/oracles/OracleRegistry.sol";
+import {IOracleRegistry} from "../src/interfaces/IChainlinkOracle.sol";
 import {FeeOptimizerAgent} from "../src/agents/FeeOptimizerAgent.sol";
 import {SlippagePredictorAgent} from "../src/agents/SlippagePredictorAgent.sol";
 import {MevHunterAgent} from "../src/agents/MevHunterAgent.sol";
@@ -23,6 +25,9 @@ contract DeploySwarm is Script {
 
         vm.startBroadcast();
 
+        // Deploy OracleRegistry for Chainlink price feeds
+        OracleRegistry oracleRegistry = new OracleRegistry();
+
         SwarmCoordinator coordinator = new SwarmCoordinator(
             IPoolManager(poolManager),
             treasury,
@@ -34,7 +39,7 @@ contract DeploySwarm is Script {
             Hooks.BEFORE_SWAP_FLAG | Hooks.AFTER_SWAP_FLAG | Hooks.AFTER_SWAP_RETURNS_DELTA_FLAG
         );
 
-        bytes memory constructorArgs = abi.encode(IPoolManager(poolManager));
+        bytes memory constructorArgs = abi.encode(IPoolManager(poolManager), IOracleRegistry(oracleRegistry));
         (address hookAddress, bytes32 salt) = HookMiner.find(
             CREATE2_DEPLOYER,
             flags,
@@ -42,7 +47,7 @@ contract DeploySwarm is Script {
             constructorArgs
         );
 
-        MevRouterHook hook = new MevRouterHook{salt: salt}(IPoolManager(poolManager));
+        MevRouterHook hook = new MevRouterHook{salt: salt}(IPoolManager(poolManager), IOracleRegistry(oracleRegistry));
         require(address(hook) == hookAddress, "hook address mismatch");
 
         FeeOptimizerAgent feeAgent = new FeeOptimizerAgent(coordinator, IPoolManager(poolManager));
@@ -60,6 +65,7 @@ contract DeploySwarm is Script {
 
         vm.stopBroadcast();
 
+        console2.log("OracleRegistry", address(oracleRegistry));
         console2.log("SwarmCoordinator", address(coordinator));
         console2.log("MevRouterHook", address(hook));
         console2.log("FeeOptimizerAgent", address(feeAgent));
